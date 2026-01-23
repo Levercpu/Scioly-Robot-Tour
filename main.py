7f5e22d13ce7034ab19c8647f929b20e24ec4168
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2026-01-23 14:36:20 -0500

    kinda expiremental

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Create your objects here.
 ev3 = EV3Brick()
 left_motor = Motor(Port.A)
-right_motor = Motor(Port.B)
+right_motor = Motor()
 gyro_sensor = GyroSensor(Port.S1)
 circumference = 20.9
 axle_track = 144
-'''drive_base = DriveBase(left_motor, right_motor, axle_track, circumference)'''
 
 
 
@@ -92,28 +91,32 @@ def turn_right(degrees,speed) :
         right_motor.stop()
     wait(0.1)
 
-'''def drive_forward(distance,speed) :
+def drive_forward(distance,maxspeed,minspeed) :
     gyro_sensor.reset_angle(0)
     left_motor.reset_angle(0)
     right_motor.reset_angle(0)
+    speed = (maxspeed-minspeed)*(((left_motor.angle()+right_motor.angle())/2 / (distance * circumference))^9)+minspeed
     while (left_motor.angle() + right_motor.angle())/2 < distance * circumference - 30 :
-        drive_base.drive(((left_motor.angle()+right_motor.angle())/2 / (distance * circumference))**9+20, -gyro_sensor.angle()/15) # this isnt actually a perfect implementation as it doesnt mimic the behavior of the block code 1:1
+        left_motor.run(speed+gyro_sensor.angle*20)
+        right_motor.run(speed-gyro_sensor.angle*20)
     left_motor.stop()
     right_motor.stop()
     wait(0.1)
-def drive_backward(distance,speed) :
+def drive_backward(distance,maxspeed,minspeed) :
     gyro_sensor.reset_angle(0)
     left_motor.reset_angle(0)
     right_motor.reset_angle(0)
+    speed = ((maxspeed-minspeed)*(((left_motor.angle()+right_motor.angle())/2 / (distance * circumference))^9)+minspeed)
     while (left_motor.angle() + right_motor.angle())/2 < distance * circumference :
-        drive_base.drive(-(((left_motor.angle()+right_motor.angle())/2 / (distance * circumference))**9+20), -gyro_sensor.angle()/15) # this isnt actually a perfect implementation as it doesnt mimic the behavior of the block code 1:1
+        left_motor.run(-speed-gyro_sensor.angle*20)
+        right_motor.run(-speed+gyro_sensor.angle*20)
     left_motor.stop()
     right_motor.stop()
-    wait(0.1)'''
+    wait(0.1)
 
 
-'''drive_forward(50,50)
-drive_backward(50,50)'''
+drive_forward(50,50)
+drive_backward(50,50)
 turn_right(90,50)
 turn_left(90,50)
 turn_180(50)
