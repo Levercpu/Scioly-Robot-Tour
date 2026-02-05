c3a799199f277c8cbf11e5cb051f7c776a72617d
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2026-02-05 16:57:47 -0500

    initial pose estimation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env pybricks-micropython
+from math import sin, cos, pi
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, UltrasonicSensor, GyroSensor
 from pybricks.parameters import Port, Stop, Direction, Button, Color
@@ -27,7 +28,7 @@ ev3 = EV3Brick()
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.D)
 gyro_sensor = GyroSensor(Port.S1)
-
+DriveBase
 #constants
 
 #cm
@@ -40,9 +41,32 @@ absolute_min_speed = 10 #smallest speed the motors will function at
 drive_speed = 500
 turn_speed = 250
 
+#ms
+dt=20
+class Robot:
+    def __init__(self, wheel_base, wheel_radius, left_motor: Motor, right_motor: Motor):
+        self.left_motor = left_motor
+        self.right_motor = right_motor
+        self.wheel_base = wheel_base
+        self.wheel_radius = wheel_radius
+        self.pos = Pose2d(0,0,0)
+        self.x = 0
+        self.y = 0
+        self.theta = 0
+    def updatePos(self):
+        averageSpeed = (left_motor.speed() + right_motor.speed())/2
+        angle = GyroSensor.angle() * pi / 180
+        self.x = self.x + averageSpeed * dt * cos(angle)
+        self.y = self.y + averageSpeed * dt * sin(angle)
+        self.theta = angle
+        self.pos = Pose2d(self.x,self.y,self.theta)
 
+class Pose2d:
+    def __init__(self, xPos, yPos, theta):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.theta = theta
 # functions
-
 
 def sign(x):
     if (x > 0):
@@ -51,6 +75,7 @@ def sign(x):
         return 0
     else:
         return -1
+    
 
 def align_angle(target_angle):
     wait(50)
@@ -59,13 +84,15 @@ def align_angle(target_angle):
         print(str(gyro_sensor.angle()) + " LEFT: " + str(left_motor.speed()) + " RIGHT: " + str(right_motor.speed()))
         left_motor.run(absolute_min_speed * sign(target_angle - gyro_sensor.angle()))
         right_motor.run(-absolute_min_speed * sign(target_angle - gyro_sensor.angle()))
+        position = position.updatePos(position)
         
     left_motor.stop()
     right_motor.stop()
 
     print(str(gyro_sensor.angle()) + " LEFT: " + str(left_motor.speed()) + " RIGHT: " + str(right_motor.speed()))
+    return position
 
-def turn(degrees, speed):
+def turn(degrees, speed, position: Pose2d) -> Pose2d:
     gyro_sensor.reset_angle(0)
         
     while abs(gyro_sensor.angle()) <= abs(degrees) - 20:
@@ -75,14 +102,16 @@ def turn(degrees, speed):
 
         left_motor.run(-turn_speed_ratio * speed * sign(gyro_sensor.angle() - degrees))
         right_motor.run(turn_speed_ratio * speed * sign(gyro_sensor.angle() - degrees))
+        position = position.updatePos(position)
     
-    align_angle(degrees)
-    align_angle(degrees)
+    align_angle(degrees, position)
+    align_angle(degrees, position)
     
     wait(100)
     print(str(gyro_sensor.angle()) + " LEFT: " + str(left_motor.speed()) + " RIGHT: " + str(right_motor.speed()))
+    return position
 
-def drive(distance, speed):
+def drive(distance, speed, position: Pose2d) -> Pose2d:
     gyro_sensor.reset_angle(0)
     left_motor.reset_angle(0)
     right_motor.reset_angle(0)
@@ -107,13 +136,16 @@ def drive(distance, speed):
             print("MAX " + str(gyro_sensor.angle()) + " LEFT: " + str(left_motor.speed()) + " RIGHT: " + str(right_motor.speed()))
             left_motor.run(p_or_n * 1.1 * speed - gyro_sensor.angle() * 5)
             right_motor.run(p_or_n * 1.1 * speed + gyro_sensor.angle() * 5)
+        position = position.updatePos(position)
 
-    align_angle(0)
-    align_angle(0)
+    align_angle(0, position)
+    align_angle(0, position)
 
     wait(100)
 
     print(str(gyro_sensor.angle()) + " LEFT: " + str(left_motor.speed()) + " RIGHT: " + str(right_motor.speed()))
+
+    return position
 
 
 def square(drive_distance, drive_speed, turn_angle, turn_speed):
